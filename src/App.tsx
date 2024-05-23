87a6885c5334d3dc99edced10f16debc0add93c2
Albert G <516972+alber70g@users.noreply.github.com>
2024-05-23 22:36:29 +0200

    working prototype

--- src/App.tsx
+++ src/App.tsx
@@ -33,11 +33,82 @@ function App() {
 
   return (
     <>
-      <NumberListLoader list={grid1} setList={setGrid1} />
-      <Grid list={grid1} setList={setGrid1} />
-      <NumberListLoader list={grid2} setList={setGrid2} />
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-bold mb-4 mt-8">
+          3D Printer UBL Mesh Tool
+        </h1>
+        <p>
+          This app allows you to modify and visualize your 3D printer's Unified
+          Bed Leveling (UBL) mesh.
+        </p>
+        <p>
+          Paste the output of your{' '}
+          <a
+            href="https://marlinfw.org/docs/gcode/M420.html"
+            className="underline"
+          >
+            <code className="text-blue-500">M420 V</code>
+          </a>{' '}
+          command
+        </p>
+        <NumberListLoader list={grid1} setList={setGrid1} />
+
+        <p>To use this tool, follow these steps:</p>
+        <ol className="list-decimal pl-4">
+          <li>
+            Modify the mesh in the left grid by clicking on the cells and
+            entering new values.
+          </li>
+          <li>View the updated mesh in the right grid.</li>
+          <li>Make any further modifications as needed.</li>
+        </ol>
+
+        <div className="flex mx-auto">
+          <div className="w-1/2">
+            <h1 className="text-2xl font-bold mb-4 mt-8">Your input mesh</h1>
+
+            <Grid list={grid1} list2={grid2} setList2={setGrid2} />
+          </div>
+          <div className="w-1/2">
+            <h1 className="text-2xl font-bold mb-4 mt-8">New mesh</h1>
+            <div className="mb-4">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => setGrid2(grid1)}
+              >
+                Reset Grid
+              </button>
+            </div>
+            <Grid list={grid2} />
+          </div>
+        </div>
+        <section className="h-lvh">
+          <h1 className="text-2xl font-bold mb-4 mt-8">G-Code</h1>
+          <p>
+            The following G-Code represents the difference between the original
+            mesh and the modified mesh:
+          </p>
+          <pre className="bg-gray-100 p-4">
+            {grid1?.[0]?.[0] && grid2?.[0]?.[0] && generateGCode(grid1, grid2)}
+          </pre>
+        </section>
+      </div>
     </>
   );
+}
+
+function generateGCode(array1: number[][], array2: number[][]) {
+  let gCode = '';
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array1[i].length; j++) {
+      const difference =
+        Math.round((array2[i][j] - array1[i][j]) * 1_000) / 1_000;
+      if (difference === 0) continue;
+      const gCodeCommand = `M421 I${i} J${j} Q${difference}\n`;
+      gCode += gCodeCommand;
+    }
+  }
+  return gCode;
 }
 
 export default App;
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -2,23 +2,48 @@ import React from 'react';
 
 type GridProps = {
   list: number[][];
-  setList: (state: number[][]) => void;
+  list2: number[][];
+  setList2?: (state: number[][]) => void;
 };
 
-const Grid: React.FC<GridProps> = ({ list, setList }) => {
+type GridPropsWithList2 = {
+  list2: number[][];
+  setList2: (state: number[][]) => void;
+} & GridProps;
+
+const Grid: React.FC<GridProps | GridPropsWithList2> = ({
+  list,
+  list2,
+  setList2,
+}) => {
+  const disableButtons = !setList2;
   const [currentRow, setCurrentRow] = React.useState<number | null>(null);
   const [currentCell, setCurrentCell] = React.useState<number | null>(null);
-  const [showButtons, setShowButtons] = React.useState<boolean>(false);
+  const [showButtons, setShowButtons] = React.useState<boolean>(
+    !disableButtons
+  );
   const handleMouseEnter = (rowIndex: number, cellIndex: number) => {
     setCurrentRow(rowIndex);
     setCurrentCell(cellIndex);
-    setShowButtons(true);
+    setShowButtons(!disableButtons);
   };
 
-  const handleMouseLeave = (rowIndex: number, cellIndex: number) => {
+  const handleMouseLeave = () => {
     setCurrentRow(null);
     setCurrentCell(null);
     setShowButtons(false);
+  };
+
+  const handleResetButtonClick = (rowIndex: number, cellIndex: number) => {
+    const originalValue = list[rowIndex][cellIndex];
+    const updatedList = list2.map((row, rIndex) =>
+      rIndex === rowIndex
+        ? row.map((cell, cIndex) =>
+            cIndex === cellIndex ? originalValue : cell
+          )
+        : row
+    );
+    setList2(updatedList);
   };
 
   const handleButtonClick = (
@@ -26,17 +51,29 @@ const Grid: React.FC<GridProps> = ({ list, setList }) => {
     cellIndex: number,
     value: number
   ) => {
-    const updatedList = list.map((row, rIndex) =>
+    console.log('update');
+    if (!list2[0]) {
+      list2 = list;
+      setList2(list2);
+    }
+    const updatedList = list2.map((row, rIndex) =>
       rIndex === rowIndex
         ? row.map((cell, cIndex) =>
             cIndex === cellIndex ? parseFloat((cell + value).toFixed(2)) : cell
           )
         : row
     );
-    setList(updatedList);
+    setList2(updatedList);
   };
 
+  const min = Math.min(...list.flat());
+  const max = Math.max(...list.flat());
 
+  const calculateHeatmapColor = (value: number) => {
+    const percentage = (value - min) / (max - min);
+    const hue = 120 - percentage * 120; // Adjust the hue range based on your preference
+    return `hsl(${hue}, 100%, 50%)`;
+  };
 
   return (
     list &&
@@ -50,15 +87,19 @@ const Grid: React.FC<GridProps> = ({ list, setList }) => {
                 <td
                   key={cellIndex}
                   className="bg-gray-200 p-2"
-                  style={{ marginRight: '10px' }}
+                  style={{
+                    marginRight: '10px',
+                    backgroundColor: calculateHeatmapColor(cell),
+                  }}
                   onMouseEnter={() => handleMouseEnter(rowIndex, cellIndex)}
-                  onMouseLeave={() => handleMouseLeave(rowIndex, cellIndex)}
+                  onMouseLeave={() => handleMouseLeave()}
                 >
                   {cell}
-                  {showButtons &&
+                  {!disableButtons &&
+                    showButtons &&
                     currentRow === rowIndex &&
                     currentCell === cellIndex && (
-                      <div className="button-container absolute">
+                      <div className="button-container absolute flex flex-col">
                         <button
                           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2"
                           onClick={() =>
@@ -82,6 +123,14 @@ const Grid: React.FC<GridProps> = ({ list, setList }) => {
                           }
                         >
                           +0.01
+                        </button>
+                        <button
+                          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2"
+                          onClick={() =>
+                            handleResetButtonClick(rowIndex, cellIndex)
+                          }
+                        >
+                          Reset
                         </button>
                         <button
                           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2"
